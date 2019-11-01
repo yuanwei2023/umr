266ac332d166fca8e41411480792f0f8c52a9b2a
Tom St Denis <tom.stdenis@amd.com>
2019-11-01 08:55:51 -0400

    add new navi14 DID
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>

--- src/lib/discover_by_did.c
+++ src/lib/discover_by_did.c
@@ -205,6 +205,7 @@ static const struct {
 	{ 0x7340, &umr_create_navi14 },
 	{ 0x7341, &umr_create_navi14 },
 	{ 0x7347, &umr_create_navi14 },
+	{ 0x734F, &umr_create_navi14 },
 	{ 0x7360, &umr_create_navi12 },
 	{ 0x7362, &umr_create_navi12 },
 	{ 0x7388, &umr_create_arcturus },
