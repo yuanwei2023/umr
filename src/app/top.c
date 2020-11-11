e8d548ac47f982c3790c2ddc497c40f1cfbffa2c
Tom St Denis <tom.stdenis@amd.com>
2020-11-11 08:35:32 -0500

    add missing gfx10 sensors
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>

--- src/app/top.c
+++ src/app/top.c
@@ -296,6 +296,10 @@ static struct umr_bitfield stat_ai_sensor_bits[] = {
 static struct umr_bitfield stat_nv_sensor_bits[] = {
 	{ "GFX_SCLK", AMDGPU_PP_SENSOR_GFX_SCLK, SENSOR_D100|(SENSOR_MHZ<<4), &umr_bitfield_default },
 	{ "GFX_MCLK", AMDGPU_PP_SENSOR_GFX_MCLK, SENSOR_D100|(SENSOR_MHZ<<4), &umr_bitfield_default },
+	{ "VDD_GFX", AMDGPU_PP_SENSOR_VDDGFX, SENSOR_MILLIVOLT<<4, &umr_bitfield_default },
+	{ "GPU_LOAD", AMDGPU_PP_SENSOR_GPU_LOAD, SENSOR_PERCENT<<4, &umr_bitfield_default },
+	{ "MEM_LOAD", AMDGPU_PP_SENSOR_MEM_LOAD, SENSOR_PERCENT<<4, &umr_bitfield_default },
+	{ "GPU_TEMP", AMDGPU_PP_SENSOR_GPU_TEMP, SENSOR_D1000|(SENSOR_TEMP<<4), &umr_bitfield_default },
 	{ NULL, 0, 0, NULL },
 };
 
