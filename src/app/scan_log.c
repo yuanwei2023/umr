d38820c575ae191d72243638f13077a148224384
Tom St Denis <tom.stdenis@amd.com>
2020-12-07 08:52:30 -0500

    enlarge mmio accelerator table for newer ASICs
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>

--- src/app/scan_log.c
+++ src/app/scan_log.c
@@ -24,7 +24,7 @@
  */
 #include "umrapp.h"
 
-#define LIST_SIZE (1UL << 18)
+#define LIST_SIZE (1ULL << 22)
 
 int umr_create_mmio_accel(struct umr_asic *asic)
 {
@@ -45,7 +45,7 @@ int umr_create_mmio_accel(struct umr_asic *asic)
 		for (j = 0; j < asic->blocks[i]->no_regs; j++) {
 			if (asic->blocks[i]->regs[j].type == REG_MMIO) {
 				if (asic->blocks[i]->regs[j].addr >= LIST_SIZE) {
-					fprintf(stderr, "[BUG]: Register address width too large for scan_log\n");
+					fprintf(stderr, "[BUG]: Register address width too large for scan_log (0x%" PRIx64 ")\n", asic->blocks[i]->regs[j].addr);
 					continue;
 				}
 				asic->mmio_accel.reglist[asic->blocks[i]->regs[j].addr] = &asic->blocks[i]->regs[j];
