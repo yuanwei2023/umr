3d6975082589ec47f89c9084f0553f4ca07bc2d8
Tom St Denis <tom.stdenis@amd.com>
2021-06-03 06:43:22 -0400

    fix NULL deref if sdma decode fails
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>

--- src/lib/read_sdma_stream.c
+++ src/lib/read_sdma_stream.c
@@ -153,13 +153,14 @@ struct umr_sdma_stream *umr_sdma_decode_stream(struct umr_asic *asic, uint64_t f
 				if (asic->family >= FAMILY_AI)
 					ps->ib.vmid |= UMR_MM_HUB;
 				ps->nwords = 5;
-
 				{
 					uint32_t *data = calloc(sizeof(*data), ps->ib.size);
 					if (umr_read_vram(asic, ps->ib.vmid, ps->ib.addr, ps->ib.size * sizeof(*data), data) == 0) {
 						ps->next_ib = umr_sdma_decode_stream(asic, from_addr + (((intptr_t)(stream - ostream)) << 2), ps->ib.vmid, data, ps->ib.size);
-						ps->next_ib->from.addr = from_addr + (((intptr_t)(stream - ostream)) << 2);
-						ps->next_ib->from.vmid = from_vmid;
+						if (ps->next_ib) {
+							ps->next_ib->from.addr = from_addr + (((intptr_t)(stream - ostream)) << 2);
+							ps->next_ib->from.vmid = from_vmid;
+						}
 					}
 					free(data);
 				}
