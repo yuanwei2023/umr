25db9db52d0a554a2122dd7b7ab62d729eba3f29
Tom St Denis <tom.stdenis@amd.com>
2021-06-22 08:49:21 -0400

    Add new VM test vectors to test new modes
    With the addition of support for new VM modes new vectors
    were required to ensure the new functionality works (and
    actually fixes things).
    
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>

--- src/test/test_vm.c
+++ src/test/test_vm.c
@@ -72,6 +72,42 @@ enum TEST_RESULT test_can_read_from_vm_memory_direct8(struct umr_asic* asic)
     return TEST_SUCCESS;
 }
 
+// three-level with PTE.further
+enum TEST_RESULT test_can_read_from_vm_memory_direct9(struct umr_asic* asic)
+{
+    uint64_t read_data = 0;
+    ASSERT_SUCCESS(umr_read_vram(asic, UMR_GFX_HUB|8, 0x7ffff6768000ULL, sizeof(read_data), &read_data));
+    ASSERT_EQ(read_data, 0x0706050403020100);
+    return TEST_SUCCESS;
+}
+
+// four-level with PTE.further
+enum TEST_RESULT test_can_read_from_vm_memory_direct10(struct umr_asic* asic)
+{
+    uint64_t read_data = 0;
+    ASSERT_SUCCESS(umr_read_vram(asic, UMR_GFX_HUB|8, 0x7f3bcca00000ULL, sizeof(read_data), &read_data));
+    ASSERT_EQ(read_data, 0x0706050403020100);
+    return TEST_SUCCESS;
+}
+
+// four-level with 2M page
+enum TEST_RESULT test_can_read_from_vm_memory_direct11(struct umr_asic* asic)
+{
+    uint64_t read_data = 0;
+    ASSERT_SUCCESS(umr_read_vram(asic, UMR_GFX_HUB|8, 0x7f26faa00000ULL, sizeof(read_data), &read_data));
+    ASSERT_EQ(read_data, 0x0706050403020100);
+    return TEST_SUCCESS;
+}
+
+// four-level with 4K page
+enum TEST_RESULT test_can_read_from_vm_memory_direct12(struct umr_asic* asic)
+{
+    uint64_t read_data = 0;
+    ASSERT_SUCCESS(umr_read_vram(asic, UMR_GFX_HUB|8, 0x7f334f600000ULL, sizeof(read_data), &read_data));
+    ASSERT_EQ(read_data, 0x0706050403020100);
+    return TEST_SUCCESS;
+}
+
 DEFINE_TESTS(vm_tests)
 #if 0
 TEST(test_can_read_from_vm_memory_direct1, "direct_vm_test1.envdef", "raven1"),
@@ -85,5 +121,9 @@ TEST(test_can_read_from_vm_memory_direct5, "direct_vm_test5.envdef", "navi10"),
 TEST(test_can_read_from_vm_memory_direct6, "direct_vm_test6.envdef", "navi10"),
 TEST(test_can_read_from_vm_memory_direct7, "direct_vm_test7.envdef", "navi10"),
 TEST(test_can_read_from_vm_memory_direct8, "direct_vm_test8.envdef", "navi10"),
+TEST(test_can_read_from_vm_memory_direct9, "direct_vm_test9.envdef", "vega10"),
+TEST(test_can_read_from_vm_memory_direct10, "direct_vm_test10.envdef", "vega10"),
+TEST(test_can_read_from_vm_memory_direct11, "direct_vm_test11.envdef", "vega10"),
+TEST(test_can_read_from_vm_memory_direct12, "direct_vm_test12.envdef", "vega10"),
 #endif
 END_TESTS(vm_tests);
