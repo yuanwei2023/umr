f72a5b6ed6b3ef53dfc32f967a057a7abf162fa2
Tom St Denis <tom.stdenis@amd.com>
2022-01-31 10:46:25 -0500

    ensure dids.sh is run on public branch
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>

--- scripts/dids.sh
+++ scripts/dids.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+cd ${pk}
+git checkout amd-staging-drm-next
+cd - > /dev/null
+
+echo
 grep 0x1002 ${pk}/amdgpu/amdgpu_drv.c | grep PCI_ANY_ID | sed -e 's/.AMD_IS_APU//g' | sed -e 's/.AMD_EXP_HW_SUPPORT//g'  | sed -e 's/.AMD_IS_MOBILITY//g' | sed -e 's/, / /g' | sed -e 's/},//g' | awk '{ print $2, $7; }' | (
 while read line; do
 	did=`echo ${line} | awk '{ print $1 }'`
