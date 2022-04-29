yuanwei2023/umr

commit 4e669e001d700cc50797f76f43db6eeefeadc150

server: disable IP discovery for now
We need the client and the server to be in sync and this cannot be
done yet with IP discovery so disable it for now.

Signed-off-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>

src/app/server.c
@@ -674,7 +674,7 @@ static void sdma_done(struct umr_sdma_stream_decode_ui *ui) {
 
 static struct umr_asic *asics[16] = {0};
 
-static void init_asics() {
+static void init_asics(int allow_ip_discovery) {
 	int i = 0;
 	struct umr_options opt = {0};
 	opt.need_scan = 1;
@@ -682,6 +682,11 @@ static void init_asics() {
 	opt.scanblock = "";
 	opt.instance = 0;
 	opt.vm_partition = -1;
+	/* Disable IP discovery in server mode for now because the client
+	 * may not be able to build the same umr_asic struct (eg: if IP discovery
+	 * is not supported or if the GPU is a different model).
+	 */
+	opt.force_asic_file = !allow_ip_discovery;
 	while ((asics[i] = umr_discover_asic(&opt, NULL))) {
 		// assign linux callbacks
 		asics[i]->mem_funcs.vm_message = dummy_printf;
@@ -912,7 +917,7 @@ JSON_Value *umr_process_json_request(JSON_Object *request)
 	}
 
 	if (asics[0] == NULL)
-		init_asics();
+		init_asics(0);
 
 	struct umr_asic *asic = NULL;
 	JSON_Object *asc = json_object_get_object(request, "asic");
@@ -1444,7 +1449,7 @@ void run_server_loop(const char *url, struct umr_asic * asic)
 	if (asic) {
 		asics[0] = asic;
 	} else {
-		init_asics();
+		init_asics(1);
 	}
 
 	/* Everything is ready. Wait for commands */

commit 2f528530750692ecdcbdac98fcfd8c35a02dc7ac

server, gui: fix IP discovery workaround
The original patch had inversed conditions and was missing
one part: the client side also shouldn't use IP discovery.

Fixes: 4e669e0 ("server: disable IP discovery for now")

src/app/server.c
@@ -674,7 +674,7 @@ static void sdma_done(struct umr_sdma_stream_decode_ui *ui) {
 
 static struct umr_asic *asics[16] = {0};
 
-static void init_asics(int allow_ip_discovery) {
+static void init_asics(int disable_ip_discovery) {
 	int i = 0;
 	struct umr_options opt = {0};
 	opt.need_scan = 1;
@@ -686,7 +686,7 @@ static void init_asics(int allow_ip_discovery) {
 	 * may not be able to build the same umr_asic struct (eg: if IP discovery
 	 * is not supported or if the GPU is a different model).
 	 */
-	opt.force_asic_file = !allow_ip_discovery;
+	opt.force_asic_file = disable_ip_discovery;
 	while ((asics[i] = umr_discover_asic(&opt, NULL))) {
 		// assign linux callbacks
 		asics[i]->mem_funcs.vm_message = dummy_printf;
@@ -916,8 +916,10 @@ JSON_Value *umr_process_json_request(JSON_Object *request)
 		goto error;
 	}
 
-	if (asics[0] == NULL)
+	if (asics[0] == NULL) {
+		/* client/server are running on the same hardware so allow IP discovery. */
 		init_asics(0);
+	}
 
 	struct umr_asic *asic = NULL;
 	JSON_Object *asc = json_object_get_object(request, "asic");
@@ -1449,6 +1451,7 @@ void run_server_loop(const char *url, struct umr_asic * asic)
 	if (asic) {
 		asics[0] = asic;
 	} else {
+		/* server may be running on a different machine so disable IP discovery. */
 		init_asics(1);
 	}
 

src/app/umr_gui.cpp
@@ -132,6 +132,8 @@ struct AsicData {
 		options.instance = instance;
 		options.database_path[0] = '\0';
 		options.no_disasm = 0;
+		/* Disable IP discovery if we're using a remote connection */
+		options.force_asic_file = lnk.use_sock;
 		asic = umr_discover_asic_by_did(&options, did, printf);
 		asic->instance = instance;
 		umr_scan_config(asic, 1);