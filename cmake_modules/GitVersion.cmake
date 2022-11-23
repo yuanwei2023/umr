yuanwei2023/umr

commit 7facf5916817723d1cc0c750b82a2fdf0579d003

CI: Release RPM and DEB packages
Release DEB, RPM and Arch Linux packages

Automatically run CI and generate a new release when
a new tag is created

TODO:
- Upload packages to package registry - Will do if requested

Signed-off-by: Galantsev, Dmitrii <dmitrii.galantsev@amd.com>

cmake_modules/GitVersion.cmake
@@ -1,0 +1,145 @@
+###############################################################################
+#
+# The University of Illinois/NCSA
+# Open Source License (NCSA)
+#
+# Copyright (c) 2014-2017, Advanced Micro Devices, Inc. All rights reserved.
+#
+# Developed by:
+#
+#                 AMD Research and AMD HSA Software Development
+#
+#                 Advanced Micro Devices, Inc.
+#
+#                 www.amd.com
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal with the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and#or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+#  - Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimers.
+#  - Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimers in
+#    the documentation and#or other materials provided with the distribution.
+#  - Neither the names of Advanced Micro Devices, Inc,
+#    nor the names of its contributors may be used to endorse or promote
+#    products derived from this Software without specific prior written
+#    permission.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS WITH THE SOFTWARE.
+#
+###############################################################################
+
+# Try to find version by looking at git tags
+#
+# Once done, this will define
+#
+# LIBDRM_FOUND
+# LIBDRM_INCLUDE_DIR
+# LIBDRM_LIBRARIES
+
+
+# Parses the VERSION_STRING variable and places
+# the first, second and third number values in
+# the major, minor and patch variables.
+function(parse_version VERSION_STRING)
+
+    string(FIND ${VERSION_STRING} "-" STRING_INDEX)
+
+    if(${STRING_INDEX} GREATER -1)
+        math(EXPR STRING_INDEX "${STRING_INDEX} + 1")
+        string(SUBSTRING ${VERSION_STRING} ${STRING_INDEX} -1 VERSION_BUILD)
+    endif()
+
+    string(REGEX MATCHALL "[0123456789]+" VERSIONS ${VERSION_STRING})
+    list(LENGTH VERSIONS VERSION_COUNT)
+
+    if(${VERSION_COUNT} GREATER 0)
+        list(GET VERSIONS 0 MAJOR)
+        set(VERSION_MAJOR
+            ${MAJOR}
+            PARENT_SCOPE)
+        set(TEMP_VERSION_STRING "${MAJOR}")
+    endif()
+
+    if(${VERSION_COUNT} GREATER 1)
+        list(GET VERSIONS 1 MINOR)
+        set(VERSION_MINOR
+            ${MINOR}
+            PARENT_SCOPE)
+        set(TEMP_VERSION_STRING "${TEMP_VERSION_STRING}.${MINOR}")
+    endif()
+
+    if(${VERSION_COUNT} GREATER 2)
+        list(GET VERSIONS 2 PATCH)
+        set(VERSION_PATCH
+            ${PATCH}
+            PARENT_SCOPE)
+        set(TEMP_VERSION_STRING "${TEMP_VERSION_STRING}.${PATCH}")
+    endif()
+
+    set(VERSION_STRING
+        "${TEMP_VERSION_STRING}"
+        PARENT_SCOPE)
+endfunction()
+
+## Gets the current version of the repository
+## using versioning tags and git describe.
+## Passes back a packaging version string
+## and a library version string.
+function(get_version_from_tag DEFAULT_VERSION_STRING)
+    parse_version(${DEFAULT_VERSION_STRING})
+
+    find_program(GIT NAMES git)
+
+    if(GIT)
+        execute_process(
+            COMMAND git describe --tags --dirty --long --match [0-9.]*
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            OUTPUT_VARIABLE GIT_TAG_STRING
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            RESULT_VARIABLE RESULT)
+        if(${RESULT} EQUAL 0)
+            parse_version(${GIT_TAG_STRING})
+        endif()
+
+        execute_process(
+            COMMAND git describe --abbrev=12 --always
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            OUTPUT_VARIABLE GIT_REV)
+
+        execute_process(
+            COMMAND git branch --show-current
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            OUTPUT_VARIABLE GIT_BRANCH)
+
+    endif()
+
+    set(VERSION_STRING
+        "${VERSION_STRING}"
+        PARENT_SCOPE)
+    set(VERSION_MAJOR
+        "${VERSION_MAJOR}"
+        PARENT_SCOPE)
+    set(VERSION_MINOR
+        "${VERSION_MINOR}"
+        PARENT_SCOPE)
+    set(VERSION_REV
+        "${VERSION_REV}"
+        PARENT_SCOPE)
+    set(VERSION_BRANCH
+        "${VERSION_BRANCH}"
+        PARENT_SCOPE)
+endfunction()

.gitlab-ci.yml
@@ -1,47 +1,231 @@
 # CI templates from
 # https://gitlab.freedesktop.org/freedesktop/ci-templates
-.templates_sha: &templates_sha 9dc112a981fb523b7c34f00b386b4e3b2e7da2c6
+.templates_sha: &templates_sha d5aa3941aa03c2f716595116354fb81eb8012acb
 
 include:
   - project: 'freedesktop/ci-templates'
     ref: *templates_sha
-    file: '/templates/alpine.yml'
+    file:
+      - '/templates/alpine.yml'
+      - '/templates/fedora.yml'
+      - '/templates/debian.yml'
+      - '/templates/arch.yml'
 
 stages:
   - build_docker
   - build
   - test
+  - release
 
-.umr_docker_vars:
-  variables:
-    FDO_DISTRIBUTION_TAG: 'built-on-2022-06-27'
-    FDO_UPSTREAM_REPO: 'tomstdenis/umr'
+variables:
+  FDO_DISTRIBUTION_VERSION: latest
+  FDO_UPSTREAM_REPO: 'tomstdenis/umr'
+  FDO_DISTRIBUTION_TAG: 'built-on-2022-10-11'
 
-build_umr_docker:
+# this is for general compilation/testing/day-to-day CI
+build_docker_alpine:
   extends:
   - .fdo.container-build@alpine
-  - .umr_docker_vars
   stage: build_docker
   variables:
-    FDO_DISTRIBUTION_PACKAGES: 'cmake g++ gcc libdrm-dev libpciaccess-dev libxml2-dev llvm llvm13 llvm13-dev llvm13-static make ncurses-dev'
+    FDO_DISTRIBUTION_PACKAGES: >
+      cmake
+      dpkg
+      g++
+      gcc
+      libdrm-dev
+      libpciaccess-dev
+      libxml2-dev
+      llvm
+      llvm13
+      llvm13-dev
+      llvm13-static
+      make
+      ncurses-dev
+      rpm-dev
 
-build:
+# build template
+.build: &build
   extends:
-  - .fdo.distribution-image@alpine
-  - .umr_docker_vars
+    - .fdo.distribution-image@alpine
   stage: build
   script:
-    - cmake -DUMR_NO_GUI=ON -B build
-    - cmake --build build -j $FDO_CI_CONCURRENT
+    - cmake -DUMR_NO_GUI=ON -G'Unix Makefiles' -B build $CMAKE_EXTRA_ARGS
+    - make -C build -j $FDO_CI_CONCURRENT $MAKE_EXTRA_ARGS
   artifacts:
     paths:
     - build/src/app/umr
 
+build:
+  <<: *build
+
 test_help:
   extends:
-  - .fdo.distribution-image@alpine
-  - .umr_docker_vars
+    - .fdo.distribution-image@alpine
   needs: [build]
   stage: test
   script:
     - ./build/src/app/umr -h
+
+
+###
+### Below instructions are only relevant for release builds
+###
+
+.release_rules:
+  rules:
+    - if: $CI_COMMIT_TAG # Run this job when a tag is created manually
+
+# this is for umr.deb release
+build_docker_debian:
+  extends:
+    - .fdo.container-build@debian
+    - .release_rules
+  stage: build_docker
+  variables:
+    FDO_DISTRIBUTION_PACKAGES: >
+      build-essential
+      cmake
+      git
+      libdrm-dev
+      libncurses-dev
+      libpciaccess-dev
+      llvm-11-dev
+      pkgconf
+      rpm
+      zlib1g-dev
+
+# this is for umr.rpm release
+build_docker_fedora:
+  extends:
+    - .fdo.container-build@fedora
+    - .release_rules
+  stage: build_docker
+  variables:
+    FDO_DISTRIBUTION_PACKAGES: >
+      cmake
+      g++
+      gcc
+      git
+      libdrm-devel
+      libpciaccess-devel
+      llvm11-devel
+      make
+      ncurses-libs
+      rpm-build
+      zlib
+
+# this is for umr.pkg.tar.zst release
+build_docker_arch:
+  extends:
+    - .fdo.container-build@arch
+    - .release_rules
+  stage: build_docker
+  variables:
+    FDO_DISTRIBUTION_PACKAGES: >
+      binutils
+      cmake
+      fakeroot
+      gcc
+      git
+      libdrm
+      llvm
+      make
+
+build_deb:
+  <<: *build
+  extends:
+    - .fdo.distribution-image@debian
+    - .release_rules
+  variables:
+    CMAKE_EXTRA_ARGS: '-DCPACK_GENERATOR=DEB'
+    MAKE_EXTRA_ARGS: 'package'
+  artifacts:
+    paths:
+    - build/umr*.deb
+
+build_rpm:
+  <<: *build
+  extends:
+    - .fdo.distribution-image@fedora
+    - .release_rules
+  variables:
+    CMAKE_EXTRA_ARGS: '-DCPACK_GENERATOR=RPM -DLLVM_DIR=/usr/lib64/llvm11/lib/cmake/llvm'
+    MAKE_EXTRA_ARGS: 'package'
+  artifacts:
+    paths:
+    - build/umr*.rpm
+
+build_zst:
+  stage: build
+  extends:
+    - .fdo.distribution-image@arch
+    - .release_rules
+  script:
+    - mkdir -p build_arch
+    # must exclude directory where the archive is being created
+    # otherwise it will fail with 'file changed as we read it'
+    - tar cvfz build_arch/umr.tar --exclude='build_arch' ./
+    - cp PKGBUILD build_arch/
+    # MUST run makepkg as user...
+    - useradd fake_user_makepkg
+    - 'chown fake_user_makepkg build_arch -R'
+    - su fake_user_makepkg -c 'cd build_arch; makepkg'
+    - mkdir -p build
+    - cp build_arch/umr*.pkg.tar.zst build/
+  artifacts:
+    paths:
+    - build/umr*.pkg.tar.zst
+
+package_please:
+  extends:
+    - .release_rules
+  # special release image, see https://docs.gitlab.com/ee/ci/yaml/#release
+  image: registry.gitlab.com/gitlab-org/release-cli:latest
+  stage: release
+  needs: ['test_help', 'build_deb', 'build_rpm', 'build_zst']
+  script:
+    - echo 'Packaging artifacts...'
+    - pwd
+    - ls -lah
+    - ls -lah build/
+    - cp build/umr*.deb ./umr-${CI_COMMIT_TAG}-Linux.deb
+    - cp build/umr*.rpm ./umr-${CI_COMMIT_TAG}-Linux.rpm
+    - cp build/umr*.pkg.tar.zst ./umr-${CI_COMMIT_TAG}-Linux.pkg.tar.zst
+  release:
+    tag_name: $CI_COMMIT_TAG
+    name: 'Release $CI_COMMIT_TAG'
+    description: '
+      Release created using the release-cli.
+
+
+      Debian/Ubuntu:
+
+
+      \`sudo apt install ./umr-${CI_COMMIT_TAG}-Linux.deb\`
+
+
+      Fedora/CentOS/RHEL:
+
+
+      \`sudo rpm -i ./umr-${CI_COMMIT_TAG}-Linux.rpm\`
+
+
+      Arch Linux:
+
+
+      \`sudo pacman -U ./umr-${CI_COMMIT_TAG}-Linux.pkg.tar.zst\`'
+    assets:
+      links:
+        - name: 'UMR DEB release'
+          url: 'https://gitlab.freedesktop.org/dmitrii.galantsev/umr/-/jobs/${CI_JOB_ID}/artifacts/file/umr-${CI_COMMIT_TAG}-Linux.deb'
+        - name: 'UMR RPM release'
+          url: 'https://gitlab.freedesktop.org/dmitrii.galantsev/umr/-/jobs/${CI_JOB_ID}/artifacts/file/umr-${CI_COMMIT_TAG}-Linux.rpm'
+        - name: 'UMR Arch Linux release'
+          url: 'https://gitlab.freedesktop.org/dmitrii.galantsev/umr/-/jobs/${CI_JOB_ID}/artifacts/file/umr-${CI_COMMIT_TAG}-Linux.pkg.tar.zst'
+
+  artifacts:
+    paths:
+      - umr-${CI_COMMIT_TAG}-Linux.deb
+      - umr-${CI_COMMIT_TAG}-Linux.rpm
+      - umr-${CI_COMMIT_TAG}-Linux.pkg.tar.zst

CMakeLists.txt
@@ -4,47 +4,44 @@ set(CPACK_RPM_COMPONENT_INSTALL ON)
 set(CPACK_DEB_COMPONENT_INSTALL ON)
 cmake_minimum_required(VERSION 3.0.1)
 
-project(umr)
+# Add local repository for FindXXX.cmake modules.
+set(CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake_modules/" ${CMAKE_MODULE_PATH})
+
+include(GitVersion)
+get_version_from_tag("1.0.0")
+
+project(umr
+    VERSION "${VERSION_STRING}"
+    HOMEPAGE_URL "https://gitlab.freedesktop.org/tomstdenis/umr")
 
 include(GNUInstallDirs)
 
-SET(MAJOR_VERSION 1)
-SET(MINOR_VERSION 0)
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "User mode register debugger for AMDGPU devices using IP cleared registers/definitions.")
+set(CPACK_PACKAGE_VENDOR "AMD")
+set(CPACK_PACKAGE_CONTACT "tom.stdenis@amd.com")
+set(CPACK_PACKAGE_DESCRIPTION_FILE "${CMAKE_CURRENT_SOURCE_DIR}/README")
+set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
+set(CPACK_RPM_PACKAGE_GROUP "System/Monitoring")
+set(CPACK_PACKAGE_VERSION_MAJOR ${VERSION_MAJOR})
+set(CPACK_PACKAGE_VERSION_MINOR ${VERSION_MINOR})
+set(CPACK_GENERATOR "RPM;DEB" CACHE STRING "Package binary using these generators")
+set(CPACK_PACKAGING_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}" CACHE STRING "Default packaging prefix." )
+mark_as_advanced(CPACK_GENERATOR)
 
-SET(CPACK_PACKAGE_DESCRIPTION_SUMMARY "User mode register debugger for AMDGPU devices using IP cleared registers/definitions.")
-SET(CPACK_PACKAGE_VENDOR "AMD")
-SET(CPACK_PACKAGE_CONTACT "tom.stdenis@amd.com")
-SET(CPACK_PACKAGE_DESCRIPTION_FILE "${CMAKE_CURRENT_SOURCE_DIR}/README")
-SET(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
-SET(CPACK_RPM_PACKAGE_GROUP "System/Monitoring")
-SET(CPACK_PACKAGE_VERSION_MAJOR ${MAJOR_VERSION})
-SET(CPACK_PACKAGE_VERSION_MINOR ${MINOR_VERSION})
-SET(CPACK_GENERATOR "RPM;DEB")
-SET(CPACK_PACKAGING_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}" CACHE STRING "Default packaging prefix." )
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libncurses6, libpciaccess-dev, libllvm11, zlib1g-dev")
 
+set(CPACK_RPM_PACKAGE_AUTOREQ 0)
+set(CPACK_RPM_PACKAGE_AUTOPROV 0)
+set(CPACK_RPM_PACKAGE_REQUIRES "ncurses-libs, libpciaccess, llvm11-libs, zlib")
 
 include(CPack)
 
-
-SET(RELEASE_VERSION \"${MAJOR_VERSION}.${MINOR_VERSION}\")
-execute_process(COMMAND  git describe --abbrev=12 --always
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    OUTPUT_VARIABLE GIT_REV
-)
-execute_process(COMMAND  git branch --show-current
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    OUTPUT_VARIABLE GIT_BRANCH
-)
+set(RELEASE_VERSION \"${VERSION_STRING}\")
 add_definitions(-DUMR_BUILD_VER=${RELEASE_VERSION})
-add_definitions(-DUMR_BUILD_REV=\"${GIT_REV}\")
-add_definitions(-DUMR_BUILD_BRANCH=\"${GIT_BRANCH}\")
+add_definitions(-DUMR_BUILD_REV=\"${VERSION_REV}\")
+add_definitions(-DUMR_BUILD_BRANCH=\"${VERSION_BRANCH}\")
 add_definitions(-DUMR_DB_DIR=\"${CMAKE_INSTALL_FULL_DATADIR}/umr/database/\")
 add_definitions(-DUMR_SOURCE_DIR=\"${CMAKE_SOURCE_DIR}\")
-
-# Add local repository for FindXXX.cmake modules.
-SET(CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake_modules/" ${CMAKE_MODULE_PATH})
 
 set(CMAKE_THREAD_PREFER_PTHREAD TRUE)
 find_package(Threads REQUIRED)

PKGBUILD
@@ -1,0 +1,30 @@
+# Original author: Emil Velikov <emil.l.velikov@gmail.com>
+# Maintainer: Dmitrii Galantsev <dmitriigalantsev@gmail.com>
+
+pkgname=umr
+epoch=1
+pkgver=1.0.0
+pkgrel=1
+pkgdesc='userspace debugging and diagnostic tool for AMD GPUs using the AMDGPU kernel driver'
+arch=('i686' 'x86_64')
+url='https://lists.freedesktop.org/archives/amd-gfx/2017-February/005122.html'
+license=('MIT')
+depends=('libpciaccess' 'ncurses' 'llvm-libs')
+makedepends=('git' 'cmake' 'llvm' 'libdrm')
+provides=('umr')
+conflicts=('umr')
+source=('umr.tar')
+sha256sums=('SKIP')
+
+pkgver() {
+	git describe --tags
+}
+
+build() {
+	cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_BUILD_TYPE=Release -DUMR_NO_GUI=ON .
+}
+
+package() {
+	make DESTDIR="$pkgdir" install
+	install -Dt "$pkgdir/usr/share/licenses/$pkgname" -m644 LICENSE
+}

cmake_modules/FindLibDRM.cmake
@@ -15,6 +15,7 @@ find_path(LIBDRM_INCLUDE_DIR NAMES amdgpu_drm.h
     ${PC_LIBDRM_INCLUDEDIR}
     ${PC_LIBDRM_INCLUDE_DIRS}
     /usr/include
+    /usr/include/libdrm
 )
 
 find_library(LIBDRM_LIBRARY NAMES libdrm_amdgpu.so.1
@@ -23,6 +24,7 @@ find_library(LIBDRM_LIBRARY NAMES libdrm_amdgpu.so.1
     ${PC_LIBDRM_LIBRARY_DIRS}
     /usr/lib64
     /usr/lib
+    /usr/lib/x86_64-linux-gnu
 )
 
 SET(LIBDRM_LIBRARIES ${LIBDRM_LIBRARY})

commit 4f5b4a6987482ca9f25596c9ec30aeabedcb8fe9

CI: Fix VERSION_REV and VERSION_BRANCH
Fix the output of the version revision and version branch, so that they're no
longer empty after the listed change below.

Cc: Dmitrii Galantsev <dmitrii.galantsev@amd.com>
Cc: Tom StDenis <tom.stdenis@amd.com>
Fixes: 7facf591681772 ("CI: Release RPM and DEB packages")
Signed-off-by: Luben Tuikov <luben.tuikov@amd.com>

cmake_modules/GitVersion.cmake
@@ -137,9 +137,9 @@ function(get_version_from_tag DEFAULT_VERSION_STRING)
         "${VERSION_MINOR}"
         PARENT_SCOPE)
     set(VERSION_REV
-        "${VERSION_REV}"
+        "${GIT_TAG_STRING}"
         PARENT_SCOPE)
     set(VERSION_BRANCH
-        "${VERSION_BRANCH}"
+        "${GIT_BRANCH}"
         PARENT_SCOPE)
 endfunction()