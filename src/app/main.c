yuanwei2023/umr

commit 04393f2a68fd886393a4fd787525dad8201334d8

umr: Add support for "--ppt-read"
Add support for "--ppt-read" to fall in line with the way rest of the
command line options are formatted. The old "--ppt_read" is still
supported.

Cc: Tom StDenis <tom.stdenis@amd.com>
Cc: Jinzhou.Su <Jinzhou.Su@amd.com>
Signed-off-by: Luben Tuikov <luben.tuikov@amd.com>
Reviewed-by: Tom St Denis <tom.stdenis@amd.com>

src/app/main.c
@@ -850,7 +850,9 @@ int main(int argc, char **argv)
 			umr_set_clock_performance(asic, "auto");
 			if (umr_check_clock_performance(asic, clockperformance, sizeof(clockperformance)) != 0)
 				printf("power_dpm_force_performance_level: %s", clockperformance);
-		} else if (!strcmp(argv[i], "--ppt_read") || !strcmp(argv[i], "-pptr")) {
+		} else if (!strcmp(argv[i], "--ppt-read") ||
+			   !strcmp(argv[i], "--ppt_read") ||
+			   !strcmp(argv[i], "-pptr")) {
 			if (!asic)
 				asic = get_asic();
 			if (i + 1 < argc) {

commit 0e76020410779ce2379410763925fe1a0d3fc2bd

umr: Fix a couple of options in the help output
The man page was updated in the listed changes below, but the help output was
forgotten.

Cc: Tom StDenis <tom.stdenis@amd.com>
Fixes: 04393f2a68fd88 ("umr: Add support for "--ppt-read"")
Fixes: dc5efe99e19028 ("umr: Add command line support for "--gpu-metrics"")
Signed-off-by: Luben Tuikov <luben.tuikov@amd.com>

src/app/main.c
@@ -1065,9 +1065,9 @@ printf(
 "\n\t--clock-high, -ch\n\t\tSet power_dpm_force_performance_level to high.\n"
 "\n\t--clock-low, -cl\n\t\tSet power_dpm_force_performance_level to low.\n"
 "\n\t--clock-auto, -ca\n\t\tSet power_dpm_force_performance_level to auto.\n"
-"\n\t--ppt_read, -pptr [ppt_field_name]\n\t\tRead powerplay table value and print it to stdout."
+"\n\t--ppt-read, -pptr [ppt_field_name]\n\t\tRead powerplay table value and print it to stdout."
 	"\n\t\tThis command will print all the powerplay table information or the corresponding string in powerplay table.\n"
-"\n\t--gpu_metrics, -gm"
+"\n\t--gpu-metrics, -gm"
 	"\n\t\tPrint the GPU metrics table for the device."
 "\n\t--power, -p \n\t\tRead the conetent of clocks, temperature, gpu loading at runtime"
 	"\n\t\toptions 'use_colour' to colourize output \n"