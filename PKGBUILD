yuanwei2023/umr

commit bd6d55a738d1c90fe94163d2812ba73037284d65

CI: update and fixup the Arch package generation
A handful of interconnected changes in here:
 - pull in the latest umr from AUR - better pkgdesc/url, no umr
   provides/conflicts, better cmake handling, rm unnecessary files
 - proper tarball type/suffix creation
 - proper package pkgver handling - don't implicitly rely on pkgver()
 - exclude the .git folder from the (temporary) tarball
 - remove pkgver() function

Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

PKGBUILD
@@ -1,30 +1,36 @@
-# Original author: Emil Velikov <emil.l.velikov@gmail.com>
+# Maintainer: Emil Velikov <emil.l.velikov@gmail.com>
 # Maintainer: Dmitrii Galantsev <dmitriigalantsev@gmail.com>
 
 pkgname=umr
-epoch=1
-pkgver=1.0.0
+pkgver=1.0.9
 pkgrel=1
-pkgdesc='userspace debugging and diagnostic tool for AMD GPUs using the AMDGPU kernel driver'
+pkgdesc='User Mode Register Debugger for AMDGPU Hardware'
 arch=('i686' 'x86_64')
-url='https://lists.freedesktop.org/archives/amd-gfx/2017-February/005122.html'
+url='https://gitlab.freedesktop.org/tomstdenis/umr'
 license=('MIT')
 depends=('libpciaccess' 'ncurses' 'llvm-libs' 'sdl2' 'nanomsg')
-makedepends=('git' 'cmake' 'llvm' 'libdrm' 'pkgconf')
-provides=('umr')
-conflicts=('umr')
-source=('umr.tar')
+makedepends=('cmake' 'llvm' 'libdrm')
+source=("$pkgname-$pkgver.tar.bz2")
 sha256sums=('SKIP')
 
-pkgver() {
-	git describe --tags
-}
-
 build() {
-	cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_BUILD_TYPE=Release .
+	local cmake_args=(
+		-B build -S "$pkgname-$pkgver"
+		-DCMAKE_INSTALL_PREFIX=/usr
+		-DCMAKE_INSTALL_LIBDIR=lib
+		-DCMAKE_BUILD_TYPE=Release
+	)
+
+	cmake "${cmake_args[@]}"
+	cmake --build build
 }
 
 package() {
-	make DESTDIR="$pkgdir" install
-	install -Dt "$pkgdir/usr/share/licenses/$pkgname" -m644 LICENSE
+	DESTDIR="$pkgdir" cmake --install build
+	# Remove some files which should not be installed. See:
+	# https://gitlab.freedesktop.org/tomstdenis/umr/-/merge_requests/32
+	rm -rf "$pkgdir/usr/bin/umrgui"
+	rm -rf "$pkgdir/usr/include"
+	rm -rf "$pkgdir/usr/lib"
+	install -Dt "$pkgdir/usr/share/licenses/$pkgname" -m644 "$pkgname-$pkgver/LICENSE"
 }

.gitlab-ci.yml
@@ -183,10 +183,10 @@ build_zst:
     - .release_rules
   script:
     - mkdir -p build
+    - sed -e "s/^pkgver.*/pkgver=${CI_COMMIT_TAG}/" PKGBUILD > build/PKGBUILD
     # must exclude directory where the archive is being created
     # otherwise it will fail with 'file changed as we read it'
-    - tar cvfz build/umr.tar --exclude='build' ./
-    - cp PKGBUILD build/
+    - tar cvfj build/umr-${CI_COMMIT_TAG}.tar.bz2 --exclude='build' --exclude='.git' ./
     # MUST run makepkg as user...
     - useradd fake_user_makepkg
     - 'chown fake_user_makepkg build -R'

commit b0750d8b575e2a33d1ff5ba0afec11f58592e6ea

PKGBUILD: use the correct source path
There is no umr-foo subfolder.

Closes: https://gitlab.freedesktop.org/tomstdenis/umr/-/issues/25
Fixes: bd6d55a ("CI: update and fixup the Arch package generation")
Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

PKGBUILD
@@ -15,7 +15,7 @@ sha256sums=('SKIP')
 
 build() {
 	local cmake_args=(
-		-B build -S "$pkgname-$pkgver"
+		-B build -S .
 		-DCMAKE_INSTALL_PREFIX=/usr
 		-DCMAKE_INSTALL_LIBDIR=lib
 		-DCMAKE_BUILD_TYPE=Release
@@ -27,5 +27,5 @@ build() {
 
 package() {
 	DESTDIR="$pkgdir" cmake --install build
-	install -Dt "$pkgdir/usr/share/licenses/$pkgname" -m644 "$pkgname-$pkgver/LICENSE"
+	install -Dt "$pkgdir/usr/share/licenses/$pkgname" -m644 "LICENSE"
 }