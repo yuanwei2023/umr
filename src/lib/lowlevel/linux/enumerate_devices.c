3e0a63b99ce12608112ba7499c392bfedf63c65e
Tom St Denis <tom.stdenis@amd.com>
2024-09-25 12:55:46 -0400

    fix segfault in enumerate when global_options is NULL
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>

--- src/lib/lowlevel/linux/enumerate_devices.c
+++ src/lib/lowlevel/linux/enumerate_devices.c
@@ -60,7 +60,7 @@ int umr_enumerate_device_list(umr_err_output errout, const char *database_path, 
 				umr_scan_config((*asics)[x], 1);
 				++x;
 
-				if (global_options->test_log && global_options->test_log_fd) {
+				if (global_options && global_options->test_log && global_options->test_log_fd) {
 					fprintf(global_options->test_log_fd, "-----\n");
 				}
 			}
