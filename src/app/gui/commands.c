d84a2bcff83c8ac72c026a181f31449b7df34c0f
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
2024-11-13 10:00:31 +0100

    server: fix build when CAN_IMPORT_BO isn't set
    Signed-off-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>

--- src/app/gui/commands.c
+++ src/app/gui/commands.c
@@ -57,6 +57,7 @@
 #include <sys/syscall.h>
 #include <amdgpu_drm.h>
 
+#if CAN_IMPORT_BO
 const char *fullscreen_vs =
 	"#version 320 es\n"
 	"precision highp float;\n"
@@ -150,6 +151,7 @@ end:
 
 	return pixels;
 }
+#endif
 
 #define SYSFS_PATH_DRM       "/sys/class/drm/"
 #define SYSFS_PATH_DEBUG_DRI "/sys/kernel/debug/dri/"
@@ -898,14 +900,16 @@ JSON_Array *parse_kms_framebuffer_sysfs_file(struct umr_asic *asic, const char *
 
 		parse_kms_field(&content, "format", "format", KMS_STRING, fb);
 		parse_kms_field(&content, "modifier", "modifier", KMS_INT_16, fb);
+		parse_kms_field(&content, "size", "size", KMS_SIZE, fb);
+
+		#if CAN_IMPORT_BO
 		uint64_t modifier_i = str_to_uint64(json_object_get_string(fb, "modifier"));
 		char *mod_str = drmGetFormatModifierName(modifier_i);
 		if (mod_str) {
 			json_object_set_string(fb, "modifier_str", mod_str);
 			free(mod_str);
 		}
-		parse_kms_field(&content, "size", "size", KMS_SIZE, fb);
-
+		#endif
 
 		JSON_Value *layers = json_value_init_array();
 		content = strstr(content, "layers:");
