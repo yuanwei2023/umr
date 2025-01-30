703983e36263e0cb0712d3d58752bdce67341e93
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
2025-01-30 13:56:33 +0100

    gui: remove gamepad support
    It's useless and slows down SDL init.
    
    Signed-off-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>

--- src/app/umr_gui.cpp
+++ src/app/umr_gui.cpp
@@ -577,7 +577,7 @@ static int run_gui(const char *url)
 		lnk.use_sock = false;
 	}
 
-	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0) {
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
 		printf("Error: %s\n", SDL_GetError());
 		return -1;
 	}
