b7a7d00c53600c3c67fc8e298db04abcd1746bc6
David (Ming Qiang) Wu <David.Wu3@amd.com>
2025-03-03 18:30:13 -0500

    vcn4: cover all vcn4 which has an extra data field
    Signed-off-by: David (Ming Qiang) Wu <David.Wu3@amd.com>

--- src/lib/vcn_enc_opcodes.c
+++ src/lib/vcn_enc_opcodes.c
@@ -474,7 +474,7 @@ static void print_ib_session_init(struct umr_asic *asic, uint32_t tvmid, uint64_
 	add_field_1(asic, tvmid, addr, &offset, "slice_output_enabled", p->slice_output_enabled, 10, pOut, pBuf);
 	add_field_1(asic, tvmid, addr, &offset, "display_remote", p->display_remote, 10, pOut, pBuf);
 
-	if ( vcn_ip_version == VCN_IP_VERSION(4, 0, 0)) {
+	if ((vcn_ip_version & VCN_IP_VERSION(4, 0, 0)) == VCN_IP_VERSION(4, 0, 0)) {
 		add_field_1(asic, tvmid, addr, &offset, "padding", *(int_field + (offset - o_offset) / 4), 0, pOut, pBuf);
 	} else {
 		STRUCT_WARNING(rvcn_enc_session_init_t);
