09c946f0e4f1ee31442474a1547808e87c32c54d
Yogesh Mohan Marimuthu <yogesh.mohanmarimuthu@amd.com>
2025-04-11 15:59:52 +0530

    only regs start CP_GFX belongs to gfx
    1 CP_HQD_PQ_CONTROL à CP_GFX_HQD_CNTL.
    2 Instead of reading the CP_GFX_HQD_RPTR, you should use the
      CP_GFX_CSMD_RPTR or both (but the CSMD one is the interesting one).
    3 You should add the CP_GFX_HQD_OFFSET as well (used to indicate where
      the graphics is resumed).
    4 None of the _EOP_ registers apply to graphics (there is no EOP buffer
      and instead the HW has an internal FIFO to queue the EOPs).
    5 None of the AQL or CWSR registers (CTX_SAVE , CNTL_STACK) exist in
      graphics.
    6 CP_HQD_DEQUEUE_REQUEST à  CP_GFX_HQD_DEQUEUE_REQUEST.
    
    Suggested-by: Mario Sopena-Novales <mario.novales@amd.com>

--- src/app/print_cpg.c
+++ src/app/print_cpg.c
@@ -73,51 +73,37 @@ void umr_print_cpg(struct umr_asic *asic)
 				if (read_banked_reg(asic, "mmCP_GFX_HQD_ACTIVE") & 0x1) {
 					uint32_t vmid = read_banked_reg(asic, "mmCP_GFX_HQD_VMID") & 0xF;
 
-					uint32_t pq_base_lo = read_banked_reg(asic, "mmCP_GFX_HQD_BASE");
-					uint32_t pq_base_hi = read_banked_reg(asic, "mmCP_GFX_HQD_BASE_HI");
-					uint64_t pq_base = ((((uint64_t)pq_base_hi) << 0x20) | pq_base_lo) << 0x8;
-					uint32_t pq_rptr = read_banked_reg(asic, "mmCP_GFX_HQD_RPTR");
-					uint32_t pq_wptr;
+					uint32_t hqd_base_lo = read_banked_reg(asic, "mmCP_GFX_HQD_BASE");
+					uint32_t hqd_base_hi = read_banked_reg(asic, "mmCP_GFX_HQD_BASE_HI");
+					uint64_t hqd_base = ((((uint64_t)hqd_base_hi) << 0x20) | hqd_base_lo) << 0x8;
+					uint32_t hqd_rptr = read_banked_reg(asic, "mmCP_GFX_HQD_RPTR");
+					uint32_t csmd_rptr = read_banked_reg(asic, "mmCP_GFX_HQD_CSMD_RPTR");
+					uint32_t hqd_wptr;
 					if (asic->family < FAMILY_AI) {
-						pq_wptr = read_banked_reg(asic, "mmCP_GFX_HQD_WPTR");
+						hqd_wptr = read_banked_reg(asic, "mmCP_GFX_HQD_WPTR");
 					} else {
-						uint32_t pq_wptr_lo = read_banked_reg(asic, "mmCP_GFX_HQD_WPTR");
-						uint32_t pq_wptr_hi = read_banked_reg(asic, "mmCP_GFX_HQD_WPTR_HI");
-						pq_wptr = (((uint64_t)pq_wptr_hi) << 0x20) | pq_wptr_lo;
+						uint32_t hqd_wptr_lo = read_banked_reg(asic, "mmCP_GFX_HQD_WPTR");
+						uint32_t hqd_wptr_hi = read_banked_reg(asic, "mmCP_GFX_HQD_WPTR_HI");
+						hqd_wptr = (((uint64_t)hqd_wptr_hi) << 0x20) | hqd_wptr_lo;
 					}
+					uint32_t hqd_rptr_addr_lo = read_banked_reg(asic, "mmCP_GFX_HQD_RPTR_ADDR");
+					uint32_t hqd_rptr_addr_hi = read_banked_reg(asic, "mmCP_GFX_HQD_RPTR_ADDR");
+					uint64_t hqd_rptr_addr = (((uint64_t)hqd_rptr_addr_hi) << 0x20) | hqd_rptr_addr_lo;
+					uint32_t hqd_cntl = read_banked_reg(asic, "mmCP_GFX_HQD_CNTL");
+					uint32_t hqd_offset = read_banked_reg(asic, "mmCP_GFX_HQD_OFFSET");
 
-					uint32_t pq_rptr_addr_lo = read_banked_reg(asic, "mmCP_HQD_PQ_RPTR_REPORT_ADDR");
-					uint32_t pq_rptr_addr_hi = read_banked_reg(asic, "mmCP_HQD_PQ_RPTR_REPORT_ADDR_HI");
-					uint64_t pq_rptr_addr = (((uint64_t)pq_rptr_addr_hi) << 0x20) | pq_rptr_addr_lo;
-					uint32_t pq_cntl = read_banked_reg(asic, "mmCP_HQD_PQ_CONTROL");
-					uint32_t eop_base_lo = read_banked_reg(asic, "mmCP_HQD_EOP_BASE_ADDR");
-					uint32_t eop_base_hi = read_banked_reg(asic, "mmCP_HQD_EOP_BASE_ADDR_HI");
-					uint64_t eop_base = ((((uint64_t)eop_base_hi) << 0x20) | eop_base_lo) << 0x8;
-					uint32_t eop_rptr = read_banked_reg(asic, "mmCP_HQD_EOP_RPTR");
-					uint32_t eop_wptr = read_banked_reg(asic, "mmCP_HQD_EOP_WPTR");
-					uint32_t eop_wptr_mem = read_banked_reg(asic, "mmCP_HQD_EOP_WPTR_MEM");
 					uint32_t mqd_base_lo = read_banked_reg(asic, "mmCP_GFX_MQD_BASE_ADDR");
 					uint32_t mqd_base_hi = read_banked_reg(asic, "mmCP_GFX_MQD_BASE_ADDR_HI");
 					uint64_t mqd_base = (((uint64_t)mqd_base_hi) << 0x20) | mqd_base_lo;
-					uint32_t deq_req = read_banked_reg(asic, "mmCP_HQD_DEQUEUE_REQUEST");
-					uint32_t iq_timer = read_banked_reg(asic, "mmCP_HQD_IQ_TIMER");
-					uint32_t aql_cntl = read_banked_reg(asic, "mmCP_HQD_AQL_CONTROL");
-					uint32_t save_base_lo = read_banked_reg(asic, "mmCP_HQD_CTX_SAVE_BASE_ADDR_LO");
-					uint32_t save_base_hi = read_banked_reg(asic, "mmCP_HQD_CTX_SAVE_BASE_ADDR_HI");
-					uint64_t save_base = (((uint64_t)save_base_hi) << 0x20) | save_base_lo;
-					uint32_t save_size = read_banked_reg(asic, "mmCP_HQD_CTX_SAVE_SIZE");
-					uint32_t stack_off = read_banked_reg(asic, "mmCP_HQD_CNTL_STACK_OFFSET");
-					uint32_t stack_size = read_banked_reg(asic, "mmCP_HQD_CNTL_STACK_SIZE");
+					uint32_t deq_req = read_banked_reg(asic, "mmCP_GFX_HQD_DEQUEUE_REQUEST");
+					uint32_t iq_timer = read_banked_reg(asic, "mmCP_GFX_HQD_IQ_TIMER");
 
 					printf("Pipe %u  Queue %u  VMID %u\n", pipe, queue, vmid);
-					printf("  PQ BASE 0x%" PRIx64 "  RPTR 0x%x  WPTR 0x%x  RPTR_ADDR 0x%" PRIx64 "  CNTL 0x%x\n",
-					pq_base, pq_rptr, pq_wptr, pq_rptr_addr, pq_cntl);
-					printf("  EOP BASE 0x%" PRIx64 "  RPTR 0x%x  WPTR 0x%x  WPTR_MEM 0x%x\n",
-					eop_base, eop_rptr, eop_wptr, eop_wptr_mem);
-					printf("  MQD 0x%" PRIx64 "  DEQ_REQ 0x%x  IQ_TIMER 0x%x  AQL_CONTROL 0x%x\n",
-					mqd_base, deq_req, iq_timer, aql_cntl);
-					printf("  SAVE BASE 0x%" PRIx64 "  SIZE 0x%x  STACK OFFSET 0x%x  SIZE 0x%x\n\n",
-					save_base, save_size, stack_off, stack_size);
+					printf("  HQD BASE 0x%" PRIx64 "  RPTR 0x%x CSMD_RPTR 0x%x WPTR 0x%x  RPTR_ADDR 0x%lx\n",
+					hqd_base, hqd_rptr, csmd_rptr, hqd_wptr, hqd_rptr_addr);
+					printf("  HQD CNTL 0x%x" PRIx64 " OFFSET 0x%x\n", hqd_cntl, hqd_offset);
+					printf("  MQD 0x%" PRIx64 "  DEQ_REQ 0x%x  IQ_TIMER 0x%x\n\n",
+					mqd_base, deq_req, iq_timer);
 				}
 			}
 
