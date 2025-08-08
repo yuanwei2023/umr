0f8b767d42a538bda35047c617fec4a0838fc95e
Tom St Denis <tom.stdenis@amd.com>
2025-08-08 14:29:06 -0400

    add readthedocs yaml file
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'UMR: User Mode Register Debugger'
-copyright = u'2020, AMD'
+copyright = u'2025, AMD'
 author = u'Tom St Denis'
 
 # The version info for the project you're documenting, acts as replacement for
