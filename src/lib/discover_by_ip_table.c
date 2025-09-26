38481a21edf093caaf8f5369e28de811484b006e
Tom St Denis <tom.stdenis@amd.com>
2025-09-26 13:08:17 -0400

    support mapping DCI blocks to DCE for older ASICs using IP discovery
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>

--- src/lib/discover_by_ip_table.c
+++ src/lib/discover_by_ip_table.c
@@ -79,6 +79,8 @@ static struct umr_ip_block *read_ip_block(struct umr_asic *asic, struct umr_disc
 		strcpy(ipcmn, "gfx");
 	} else if (!strcmp(det->ipname, "uvd")) { // for any IP that has discovery UVD == VCN
 		strcpy(ipcmn, "vcn");
+	} else if (!strcmp(det->ipname, "dci")) {
+		strcpy(ipcmn, "dce");
 	} else if (!strcmp(det->ipname, "dmu")) {
 		strcpy(ipcmn, "dcn");
 	} else if (!strcmp(det->ipname, "nbif")) {
@@ -322,6 +324,8 @@ struct umr_asic *umr_discover_asic_by_discovery_table(char *aname, struct umr_op
 			strcpy(cmnname, "sdma");
 		} else if (!strcmp(det->ipname, "uvd")) {
 			strcpy(cmnname, "vcn");
+		} else if (!strcmp(det->ipname, "dci")) {
+			strcpy(cmnname, "dce");
 		} else if (!strcmp(det->ipname, "dmu")) {
 			strcpy(cmnname, "dcn");
 		} else if (!strcmp(det->ipname, "nbif")) {
