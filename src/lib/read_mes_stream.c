28206064eff57b114dc09486c066cee2fd7c6e9e
Tom St Denis <tom.stdenis@amd.com>
2025-09-29 06:35:30 -0400

    add new MES v11 and v12 bits
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>

--- src/lib/read_mes_stream.c
+++ src/lib/read_mes_stream.c
@@ -367,6 +367,7 @@ struct umr_mes_stream *umr_mes_decode_stream_opcodes(struct umr_asic *asic, stru
 						ui->add_field(ui, ib_addr + 4 * i, ib_vmid, "enable_mes_sch_stb_log", (fetch_word(asic, stream, i) >> 16) & 1, NULL, 10, 32);
 						ui->add_field(ui, ib_addr + 4 * i, ib_vmid, "limit_single_process", (fetch_word(asic, stream, i) >> 17) & 1, NULL, 10, 32);
 						ui->add_field(ui, ib_addr + 4 * i, ib_vmid, "is_strix_tmz_wa_enabled", (fetch_word(asic, stream, i) >> 18) & 1, NULL, 10, 32);
+						ui->add_field(ui, ib_addr + 4 * i, ib_vmid, "enable_lr_compute_wa", (fetch_word(asic, stream, i) >> 19) & 1, NULL, 10, 32);
 					} else 	if (mes_ver_maj == 12) {
 						ui->add_field(ui, ib_addr + 4 * i, ib_vmid, "use_disable_queue_in_legacy_uq_preemption", (fetch_word(asic, stream, i) >> 11) & 1, NULL, 10, 32);
 						ui->add_field(ui, ib_addr + 4 * i, ib_vmid, "send_write_data", (fetch_word(asic, stream, i) >> 12) & 1, NULL, 10, 32);
@@ -377,6 +378,9 @@ struct umr_mes_stream *umr_mes_decode_stream_opcodes(struct umr_asic *asic, stru
 						ui->add_field(ui, ib_addr + 4 * i, ib_vmid, "enable_mes_sch_stb_log", (fetch_word(asic, stream, i) >> 17) & 1, NULL, 10, 32);
 						ui->add_field(ui, ib_addr + 4 * i, ib_vmid, "limit_single_process", (fetch_word(asic, stream, i) >> 18) & 1, NULL, 10, 32);
 						ui->add_field(ui, ib_addr + 4 * i, ib_vmid, "unmapped_doorbell_handling", (fetch_word(asic, stream, i) >> 19) & 3, NULL, 10, 32);
+						ui->add_field(ui, ib_addr + 4 * i, ib_vmid, "enable_mes_fence_int", (fetch_word(asic, stream, i) >> 21) & 1, NULL, 10, 32);
+						ui->add_field(ui, ib_addr + 4 * i, ib_vmid, "enable_lr_compute_wa", (fetch_word(asic, stream, i) >> 22) & 1, NULL, 10, 32);
+
 					}
 				}
 				++i;
