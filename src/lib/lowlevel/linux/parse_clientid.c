75e476e562ec171206b7671631c286f463149cc2
Tom St Denis <tom.stdenis@amd.com>
2025-11-11 11:36:26 -0500

    gfx12 SDMA was the right way around originally
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>

--- src/lib/lowlevel/linux/parse_clientid.c
+++ src/lib/lowlevel/linux/parse_clientid.c
@@ -501,7 +501,7 @@ static int init_gfx12_queue(struct umr_asic *asic, int x, int *init)
             asic->options.user_queue.client_info.queue[x].hqd_base_addr =
                 (((uint64_t)mqdwords[2] << 32) | mqdwords[1]) << 8; // sdmax_rlcx_rb_base
             asic->options.user_queue.client_info.queue[x].hqd_rptr_addr =
-                (((uint64_t)mqdwords[7] << 32) | mqdwords[8]); // sdmax_rlcx_rb_rptr_addr_lo
+                (((uint64_t)mqdwords[8] << 32) | mqdwords[7]); // sdmax_rlcx_rb_rptr_addr_lo
             asic->options.user_queue.client_info.queue[x].rb_wptr_poll_addr =
                 (((uint64_t)mqdwords[25] << 32) | mqdwords[24]); // sdmax_rlcx_rb_wptr_poll_addr_lo
 
