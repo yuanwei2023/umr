d10725974a1228585d790b140ef44942a7ee4e8a
Tom St Denis <tom.stdenis@amd.com>
2025-12-08 11:17:24 -0500

    coverity: src/lib/packet/vcn/read_vcn_dec_stream.c cleaned up
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>

--- src/lib/packet/vcn/read_vcn_dec_stream.c
+++ src/lib/packet/vcn/read_vcn_dec_stream.c
@@ -291,23 +291,27 @@ struct umr_pm4_stream *umr_vcn_dec_decode_stream(struct umr_asic *asic, uint32_t
 							uint32_t size = sizeof(rvcn_dec_message_header_t);
 							rvcn_dec_message_header_t *mh = calloc(1, size);
 							vcn = calloc(1, sizeof(struct umr_vcn_cmd_message));
-							vcn->vmid = vmid;
-							vcn->addr = dec_ib.addr;
-							vcn->cmd = dec_ib.cmd;
-							vcn->type = 0;
-							vcn->from = (stream - ostream  - 1) * 4;  /* back 1 dwords to mmUVD_GPCOM_VCPU_DATA1 */
-							if (umr_read_vram(asic, asic->options.vm_partition, vcn->vmid, vcn->addr, size, mh) < 0) {
-								asic->err_msg("[ERROR]: Could not read IB at 0x%"PRIx32":0x%" PRIx64 "\n", vcn->vmid, vcn->addr);
-								free(vcn);
+							if (!vcn) {
+								asic->err_msg("[ERROR]: Could not allocate memory for rcvn_dec_message_header\n");
 							} else {
-								vcn->size = mh->total_size < mh->header_size ? mh->header_size : mh->total_size;
-								if (!ops->vcn)
-									ops->vcn = vcn;
-								else
-									vcn_head->next = vcn;
-								vcn_head = vcn;
+								vcn->vmid = vmid;
+								vcn->addr = dec_ib.addr;
+								vcn->cmd = dec_ib.cmd;
+								vcn->type = 0;
+								vcn->from = (stream - ostream  - 1) * 4;  /* back 1 dwords to mmUVD_GPCOM_VCPU_DATA1 */
+								if (umr_read_vram(asic, asic->options.vm_partition, vcn->vmid, vcn->addr, size, mh) < 0) {
+									asic->err_msg("[ERROR]: Could not read IB at 0x%"PRIx32":0x%" PRIx64 "\n", vcn->vmid, vcn->addr);
+									free(vcn);
+								} else {
+									vcn->size = mh->total_size < mh->header_size ? mh->header_size : mh->total_size;
+									if (!ops->vcn)
+										ops->vcn = vcn;
+									else
+										vcn_head->next = vcn;
+									vcn_head = vcn;
+								}
+								free(mh);
 							}
-							free(mh);
 						}
 					}
 					/* reset for next IB message if any */
