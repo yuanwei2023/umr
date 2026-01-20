5d8b86a58dbba211979a69e168dbca7cdd953baf
Tom St Denis <tom.stdenis@amd.com>
2026-01-20 09:30:52 -0500

    remove error message from umr_read_sensor() since there is no way to query a sensors presence
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>

--- src/lib/lowlevel/linux/read_sensor.c
+++ src/lib/lowlevel/linux/read_sensor.c
@@ -33,7 +33,6 @@ int umr_read_sensor(struct umr_asic *asic, int sensor, void *dst, int *size)
 
 	// multiply sensor index by 4 to get byte address
 	if (lseek(asic->fd.sensors, sensor*4, SEEK_SET) == -1 || (r = read(asic->fd.sensors, dst, *size)) != *size) {
-		asic->err_msg("[ERROR]: Could not seek or read from sensors debugfs file\n");
 		return -1;
 	}
 	*size = r;
