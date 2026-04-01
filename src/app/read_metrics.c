dce81e41c0cc26618bfab3f4a4fef936bd53f804
Tom St Denis <tom.stdenis@amd.com>
2026-04-01 09:56:56 -0400

    port umr_print_gpu_metrics() over to new kernel binary format
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>

--- src/app/read_metrics.c
+++ src/app/read_metrics.c
@@ -26,43 +26,30 @@
 
 int umr_print_gpu_metrics(struct umr_asic *asic, int delay)
 {
-	FILE *f = NULL;
+	int fd;
 	uint8_t *pp_data = NULL;
-	size_t size;
+	ssize_t size;
 	char pp_name[128];
 	int r;
 
 	snprintf(pp_name, sizeof(pp_name), "/sys/class/drm/card%d/device/gpu_metrics", asic->instance);
-	f = fopen(pp_name, "rb");
-	if (!f) {
+	fd = open(pp_name, O_RDONLY);
+	if (fd < 0) {
 		asic->err_msg("[ERROR]:  Cannot open gpu_metrics file %s\n", pp_name);
 		return -1;
 	}
-	if (fseek(f, 0, SEEK_END) < 0) {
-		asic->err_msg("[ERROR]:  Cannot seek in gpu_metrics debugfs file\n");
-		fclose(f);
+	size = 128 * 1024; // default to max of 128KB
+	pp_data = calloc(size, sizeof *pp_data);
+	if (!pp_data) {
+		asic->err_msg("[ERROR]: Out of memory\n");
+		close(fd);
 		return -1;
 	}
-	size = ftell(f);
-	if (size > 0) {
-		fseek(f, 0, SEEK_SET);
-		pp_data = calloc(size, sizeof *pp_data);
-		if (!pp_data) {
-			asic->err_msg("[ERROR]: Out of memory\n");
-			fclose(f);
-			return -1;
-		}
-	} else {
-		asic->err_msg("[ERROR]: Could not seek in gpu_metrics file\n");
-		fclose(f);
-		return -1;
-	}
-
 	r = 0;
 	do {
 		struct umr_key_value *kv;
 		int x;
-		if (fread(pp_data, 1, size, f) == size) {
+		if ((size = read(fd, pp_data, size)) > 0) {
 			kv = umr_dump_metrics(asic, pp_data, size);
 			if (!kv) {
 				r = -1;
@@ -74,7 +61,7 @@ int umr_print_gpu_metrics(struct umr_asic *asic, int delay)
 			free(kv);
 			if (delay) {
 				usleep(abs(delay) * 1000UL);
-				fseek(f, 0, SEEK_SET);
+				lseek(fd, 0, SEEK_SET);
 			}
 		} else {
 			asic->err_msg("[ERROR]: Could not access gpu_metrics file\n");
@@ -82,7 +69,7 @@ int umr_print_gpu_metrics(struct umr_asic *asic, int delay)
 		}
 	} while (delay);
 error:
-	fclose(f);
+	close(fd);
 	free(pp_data);
 	return r;
 }
