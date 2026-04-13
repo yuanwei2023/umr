54c9072f477db33ca4ac4814359960cf543dc927
Kent Russell <kent.russell@amd.com>
2026-04-13 13:43:58 -0400

    scripts: Add wait_and_dump_all.sh script
    This script will wait for a hang, and dump the UMR logs automatically,
    and then restore halt_if_hws_hang=0 to resume the hang handling. This
    can be run in a separate terminal and will do the UMR dump as soon as we
    detect one of the 3 hang messages in dmesg
    
    Signed-off-by: Kent Russell <kent.russell@amd.com>

---
+++ scripts/diag/wait_and_dump_all.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+
+#figure out where scripts are installed and source functions
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+#relaunch ourself as root
+if [ `whoami` != root ]; then
+    sudo ${dir}/wait_and_dump_all.sh "$@"
+    exit 0
+fi
+
+echo 1 | sudo tee /sys/module/amdgpu/parameters/halt_if_hws_hang
+
+echo "Waiting for failure..."
+
+in_reset=0
+
+stdbuf -oL -eL journalctl -b -f | while IFS= read -r line; do
+
+    if [[ "$line" == *"GPU reset begin!"* ]]; then
+        in_reset=1
+    fi
+    if [[ "$line" =~ GPU\ reset.*\ succeeded! ]]; then
+        in_reset=0
+    fi
+
+    if [[ "$line" == *"qcm fence wait"* || \
+          "$line" == *"preemption failed"* || \
+          "$line" == *"failed to respond"* ]]; then
+        if [[ $in_reset -eq 0 ]]; then
+            ./dump_all_cpc_info.sh
+        fi
+        break
+    fi
+done
+
+# Setting halt_if_hws_hang back to 0 will un-halt the hang and get things running again without needing a reset
+echo 0 | sudo tee /sys/module/amdgpu/parameters/halt_if_hws_hang
