6255dbe27d6fa8a98b54e0213fa6a186adbf06ad
Tom St Denis <tom.stdenis@amd.com>
2026-05-19 08:58:10 -0400

    fix reg names for scanning waves
    Signed-off-by: Tom St Denis <tom.stdenis@amd.com>

--- src/lib/scan_waves.c
+++ src/lib/scan_waves.c
@@ -747,7 +747,7 @@ struct umr_wave_data *umr_scan_wave_data(struct umr_asic *asic)
 			}
 		} else {
 			uint32_t wgp_mask = 0xFFFFFFFF;
-			uint32_t wgp_width = umr_bitwidth_reg_by_name_by_ip_by_instance(asic, "gfx", asic->options.vm_partition, "CC_GC_SHADER_ARRAY_CONFIG", "INACTIVE_WGPS");
+			uint32_t wgp_width = umr_bitwidth_reg_by_name_by_ip_by_instance(asic, "gfx", asic->options.vm_partition, "regCC_GC_SHADER_ARRAY_CONFIG", "INACTIVE_WGPS");
 			struct umr_options options = asic->options;
 
 			asic->options.use_bank = 1;				// index by GRBM
@@ -756,10 +756,10 @@ struct umr_wave_data *umr_scan_wave_data(struct umr_asic *asic)
 			asic->options.bank.grbm.instance = 0;		// instance 0
 						// read CC_GC_SHADER_ARRAY_CONFIG and GC_USER_SHADER_ARRAY_CONFIG, OR together, and skip wgp's with a corresponding 1 bit
 			wgp_mask =
-				umr_bitslice_reg_by_name_by_ip_by_instance(asic, "gfx", asic->options.vm_partition, "CC_GC_SHADER_ARRAY_CONFIG", "INACTIVE_WGPS", 
-					umr_read_reg_by_name_by_ip_by_instance(asic, "gfx", asic->options.vm_partition, "CC_GC_SHADER_ARRAY_CONFIG")) |
-				umr_bitslice_reg_by_name_by_ip_by_instance(asic, "gfx", asic->options.vm_partition, "GC_USER_SHADER_ARRAY_CONFIG", "INACTIVE_WGPS", 
-					umr_read_reg_by_name_by_ip_by_instance(asic, "gfx", asic->options.vm_partition, "GC_USER_SHADER_ARRAY_CONFIG"));
+				umr_bitslice_reg_by_name_by_ip_by_instance(asic, "gfx", asic->options.vm_partition, "regCC_GC_SHADER_ARRAY_CONFIG", "INACTIVE_WGPS",
+					umr_read_reg_by_name_by_ip_by_instance(asic, "gfx", asic->options.vm_partition, "regCC_GC_SHADER_ARRAY_CONFIG")) |
+				umr_bitslice_reg_by_name_by_ip_by_instance(asic, "gfx", asic->options.vm_partition, "regGC_USER_SHADER_ARRAY_CONFIG", "INACTIVE_WGPS",
+					umr_read_reg_by_name_by_ip_by_instance(asic, "gfx", asic->options.vm_partition, "regGC_USER_SHADER_ARRAY_CONFIG"));
 
 			asic->options = options;
 
